9bf0f40028e144c817e4a11a07f887f408504acb
mflorencio-inteli <marcos.silva@sou.inteli.edu.br>
2022-05-11 13:52:13 -0300

    versao semifinal do front end

--- src/Frontend/public/projects.js
+++ src/Frontend/public/projects.js
@@ -2,106 +2,148 @@ var $table = $('#table');
 var mydata = 
 [
     {
-        "id": 0,
-        "name": "test0",
-        "demanded hours": "400h",
-        "conclusion": "20%"
-    },
-    {
         "id": 1,
-        "name": "test1",
-        "demanded hours": "300h",
-        "conclusion": "40%"
+        "name": "alpha",
+        "demanded hours": "80",
+        "conclusion": "13%",
+        "accountable": "Naruto"
     },
     {
         "id": 2,
-        "name": "test2",
-        "demanded hours": "250h",
-        "conclusion": "50%"
+        "name": "Beta",
+        "demanded hours": "55",
+        "conclusion": "21%",
+        "accountable": "Sasuki",
     },
     {
         "id": 3,
-        "name": "test3",
-        "demanded hours": "350h",
-        "conclusion": "20%"
+        "name": "Gamma",
+        "demanded hours": "72",
+        "conclusion": "50%",
+        "accountable": "Gaara",
     },
     {
         "id": 4,
-        "name": "test4",
-        "demanded hours": "450h",
-        "conclusion": "10%"
+        "name": "Delta",
+        "demanded hours": "103",
+        "conclusion": "41%",
+        "accountable": "Deidara",
     },
     {
         "id": 5,
-        "name": "test5",
-        "demanded hours": "350h",
-        "conclusion": "70%"
+        "name": "Epsilon",
+        "demanded hours": "42",
+        "conclusion": "52%",
+        "accountable": "Pain",
     },
     {
         "id": 6,
-        "name": "test6",
-        "demanded hours": "320h",
-        "conclusion": "20%"
+        "name": "Zeta",
+        "demanded hours": "21",
+        "conclusion": "18%",
+        "accountable": "Kakashi",
     },
     {
         "id": 7,
-        "name": "test7",
-        "demanded hours": "435h",
-        "conclusion": "30%"
+        "name": "Eta",
+        "demanded hours": "57",
+        "conclusion": "69%",
+        "accountable": "Naruto",
     },
     {
         "id": 8,
-        "name": "test8",
-        "demanded hours": "380h",
-        "conclusion": "80%"
+        "name": "Theta",
+        "demanded hours": "93",
+        "conclusion": "24%",
+        "accountable": "Sasuki",
     },
     {
         "id": 9,
-        "name": "test9",
-        "demanded hours": "480h",
-        "conclusion": "20%"
+        "name": "Iota",
+        "demanded hours": "98",
+        "conclusion": "56%",
+        "accountable": "Gaara"
     },
     {
         "id": 10,
-        "name": "test10",
-        "demanded hours": "380h",
-        "conclusion": "10%"
+        "name": "Kappa",
+        "demanded hours": "52",
+        "conclusion": "92%",
+        "accountable": "Deidara",
     },
     {
         "id": 11,
-        "name": "test10",
-        "demanded hours": "560h",
-        "conclusion": "60%"
+        "name": "Lambda",
+        "demanded hours": "23",
+        "conclusion": "88%",
+        "accountable": "Pain",
+
     },
     {
         "id": 12,
-        "name": "test10",
-        "demanded hours": "580h",
-        "conclusion": "10%"
+        "name": "MU",
+        "demanded hours": "49",
+        "conclusion": "13%",
+        "accountable": "Kakashi",
     },
     {
         "id": 13,
-        "name": "test10",
-        "demanded hours": "280h",
-        "conclusion": "90%"
+        "name": "Nu",
+        "demanded hours": "19",
+        "conclusion": "67%",
+        "accountable": "Naruto",
     },
     {
         "id": 14,
-        "name": "test10",
-        "demanded hours": "470h",
-        "conclusion": "10%"
+        "name": "Xi",
+        "demanded hours": "7",
+        "conclusion": "45%",
+        "accountable": "Sasuki",
     },
     {
         "id": 15,
-        "name": "test10",
-        "demanded hours": "320h",
-        "conclusion": "10%"
+        "name": "Omicron",
+        "demanded hours": "77",
+        "conclusion": "93%",
+        "accountable": "Gaara",
     },
     {
         "id": 16,
-        "name": "test10",
-        "demanded hours": "580h",
-        "conclusion": "10%"
+        "name": "Pi",
+        "demanded hours": "42",
+        "conclusion": "17%",
+        "accountable": "Deidara",
+    },
+    {
+        "id": 17,
+        "name": "Rho",
+        "demanded hours": "61",
+        "conclusion": "32%",
+        "accountable": "Pain",
+    },
+
+    {
+        "id": 18,
+        "name": "Sigma",
+        "demanded hours": "94",
+        "conclusion": "89%",
+        "accountable": "Kakashi",
+    },
+
+    {
+        "id": 19,
+        "name": "Tau",
+        "demanded hours": "29",
+        "conclusion": "41%",
+        "accountable": "Gaara",
+    },
+
+    {
+        "id": 20,
+        "name": "Epsilon",
+        "demanded hours": "11",
+        "conclusion": "84%",
+        "accountable": "Deidara",
     },
 ];
 
