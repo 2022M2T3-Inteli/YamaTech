3c233209000736c4c537f3e9a8e8bf94930f1e00
mflorencio-inteli <marcos.silva@sou.inteli.edu.br>
2022-06-09 13:48:23 -0300

    novo commit

--- teste.js
+++ teste.js
@@ -1,6 +1,25 @@
-var a = "(40, 20, 30)";
+var a = [
+    {
+        "Jul-2022": 21.666666666666668,
+        "Mon Aug 01 2022 14:23:10 GMT-0300 (Brasilia Standard Time)": 21.666666666666668,
+        "Thu Sep 01 2022 14:23:10 GMT-0300 (Brasilia Standard Time)": 24.416666666666668,
+        "Sat Oct 01 2022 14:23:10 GMT-0300 (Brasilia Standard Time)": 27.166666666666668,
+        "Tue Nov 01 2022 14:23:10 GMT-0300 (Brasilia Standard Time)": 27.166666666666668,
+        "Thu Dec 01 2022 14:23:10 GMT-0300 (Brasilia Standard Time)": 29.916666666666668,
+        "Sun Jan 01 2023 14:23:10 GMT-0300 (Brasilia Standard Time)": 8.25,
+        "Wed Feb 01 2023 14:23:10 GMT-0300 (Brasilia Standard Time)": 11,
+        "Wed Mar 01 2023 14:23:10 GMT-0300 (Brasilia Standard Time)": 8.25,
+        "Sat Apr 01 2023 14:23:10 GMT-0300 (Brasilia Standard Time)": 5.5
+    },
+    {
+        "legal_hours": 704,
+        "total_hours": 550,
+        "allocated_hours": 365
+    }
+]
 
-a = a.replace("(", "").replace(")", "").split(",");
-console.log(a);
-console.log(typeof a);
-console.log(typeof a[0]);
+
+var b = new Date();
+console.log(b);
+console.log(b.getMonth());
+console.log(b.getDate());
