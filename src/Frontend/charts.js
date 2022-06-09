13ce58884c22a7e4ae1c1d4e105aea9465a51a06
Leandro de Oliveira Custódio <leandro.custodio@sou.inteli.edu.br>
2022-06-09 13:31:08 -0300

    dashboard e graficos

--- src/Frontend/charts.js
+++ src/Frontend/charts.js
@@ -1,70 +1,137 @@
+var a = [
+    {
+        "Fri Jul 01 2022 14:23:10 GMT-0300 (Brasilia Standard Time)": 21.666666666666668,
+        "Mon Aug 01 2022 14:23:10 GMT-0300 (Brasilia Standard Time)": 21.666666666666668,
+        "Thu Sep 01 2022 14:23:10 GMT-0300 (Brasilia Standard Time)": 24.416666666666668,
+        "Sat Oct 01 2022 14:23:10 GMT-0300 (Brasilia Standard Time)": 27.166666666666668,
+        "Tue Nov 01 2022 14:23:10 GMT-0300 (Brasilia Standard Time)": 27.166666666666668,
+        "Thu Dec 01 2022 14:23:10 GMT-0300 (Brasilia Standard Time)": 29.916666666666668,
+        "Sun Jan 01 2023 14:23:10 GMT-0300 (Brasilia Standard Time)": 8.25,
+        "Wed Feb 01 2023 14:23:10 GMT-0300 (Brasilia Standard Time)": 11,
+        "Wed Mar 01 2023 14:23:10 GMT-0300 (Brasilia Standard Time)": 8.25,
+        "Sat Apr 01 2023 14:23:10 GMT-0300 (Brasilia Standard Time)": 5.5
+    },
+    {
+        "legal_hours": 704,
+        "total_hours": 550,
+        "allocated_hours": 365
+    }
+]
+
+//console.log(a[1]["legal_hours"]);
+function legal_hours() {
+var lhours = [];
+
+var labels = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+var array = '[';
+for (let i=0; i < labels.length; i++) {
+    lhours.push(a[1]["legal_hours"]);
+    if(i == labels.length-1)    array += a[1]["legal_hours"] + ']';
+    else array += a[1]["legal_hours"] + ',';
+    console.log(a[1]["legal_hours"]);
+    }
+    console.log(array);
+    return lhours;
+}
+
+function total_hours() {
+    var thours = [];
+
+    var labels = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+    var array = '[';
+    for (let i=0; i < labels.length; i++) {
+        thours.push(a[1]["total_hours"]);
+        if(i == labels.length-1)    array += a[1]["total_hours"] + ']';
+        else array += a[1]["total_hours"] + ',';
+        console.log(a[1]["total_hours"]);
+        }
+        console.log(array);
+        return thours;
+}
+
+function allocated_hours() {
+    var ahours = [];
+
+    var labels = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+    var array = '[';
+    for (let i=0; i < labels.length; i++) {
+        ahours.push(a[1]["allocated_hours"]);
+        if(i == labels.length-1)    array += a[1]["allocated_hours"] + ']';
+        else array += a[1]["allocated_hours"] + ',';
+        console.log(a[1]["allocated_hours"]);
+        }
+        console.log(array);
+        return ahours;
+    
+}
+
+
+allocated_hours();
+total_hours();
+legal_hours();
+
+
 const ctx = document.getElementById('myChart').getContext('2d');
 const ctx2 = document.getElementById('myChart2').getContext('2d');
-const ctx3 = document.getElementById('myChart3').getContext('2d');
-const ctx4 = document.getElementById('myChart4').getContext('2d');
 
 
-// Chart 1
+
+//Chart 1
+function grafico(teste) {
+
+
+}
 const myChart = new Chart(ctx, {
-    type: 'polarArea',
+    type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        type: 'line',
+        labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
         datasets: [{
-            label: '# of Votes',
-            data: [42, 39, 30, 50, 27, 30],
+        label: 'Limite de Horas Disponíveis',
+            type: 'line',
+            data: legal_hours(),
+            backgroundColor: "rgb(255,0,0)",
+            fill: false,
+            borderColor: 'rgb(255, 0, 0)'
+
+        },
+        {
+            label: 'Limite de Horas Disponíveis',
+                type: 'line',
+                data: total_hours(),
+                backgroundColor: "rgb(0, 250, 20)",
+                fill: false,
+                borderColor: 'rgb(0, 250, 20)'
+               
+        },
+        {
+            label: 'Limite de Horas Disponíveis',
+                type: 'line',
+                data: allocated_hours(),
+                backgroundColor: "rgb(0, 200, 255)",
+                fill: false,
+                borderColor: 'rgb(0, 200, 255)'
+               
+        },
+        {
+        label: 'Horas Necessárias Por Projetos',
+            data: [690, 560, 400, 480, 800, 712, 1014 , 580, 777, 200, 400, 200],
             backgroundColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ]
+                'rgba(54,120,230,1)',
+            ],
+            fill: true,
+            backgroundColor: 'rgba(54,120,230,0.5)',
         }]
     },
     options: {
        responsive: true,
     }
+    
 });
+
 
 //Chart 2
 const myChart2 = new Chart(ctx2, {
-    type: 'line',
-    data: {
-        labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
-        datasets: [{
-            label: 'Horas Necessárias Por Projetos',
-            data: [12, 19, 3, 5, 2, 3, 8 , 5, 9, 10, 47, 4],
-            backgroundColor: [
-                'rgba(54,120,230,1)',
-            ]
-        }]
-    },
-    options: {
-       responsive: true,
-    }
-});
-
-//Chart 3
-const myChart3 = new Chart(ctx3, {
-    type: 'bar',
-    data: {
-        labels: ['Php', 'Phyton', 'JavaScript', 'Ux'],
-        datasets: [{
-            label: 'Especialidades',
-            data: [25, 11, 19, 55],
-            backgroundColor: [
-                'rgba(54,120,230,1)',
-            ]
-        }]
-    },
-    options: {
-       responsive: true,
-    }
-});
-
-//Chart 4
-const myChart4 = new Chart(ctx4, {
     type: 'bar',
     data: {
         labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
@@ -73,11 +140,16 @@ const myChart4 = new Chart(ctx4, {
             data: [12, 19, 3, 5, 2, 3, 8 , 5, 9, 10, 47, 4],
             backgroundColor: [
                 'rgba(54,120,230,1)',
-            ]
+            ],
+            backgroundColor: 'rgba(54,120,230,0.5)',
         }]
+        
     },
     options: {
        responsive: true,
-    }
+    },
+    
+            
 });
+
 
