faa1ed87be0ccc7f3f6442aaad8082c1a7f9c022
mflorencio-inteli <marcos.silva@sou.inteli.edu.br>
2022-06-11 14:39:40 -0300

    ajustes de nomenclatura de docs

--- src/frontend/employees.js
+++ src/frontend/employees.js
@@ -76,6 +76,41 @@ function getEmployeesList() {
 }
 
 
+function postEmp() {
+    var full_name = document.getElementById("full_name_form").value;
+    var position = document.getElementById("position_form").value;
+    var legal_hours = document.getElementById("legal_hours_form").value;
+    var total_hours = document.getElementById("total_hours_form").value;
+    var allocated_hours = document.getElementById("allocated_hours_form").value;
+    var outsourced = document.getElementById("outsourced_form").value;
+    var local = document.getElementById("local_form").value;
+    var isActive = 1;
+
+    var url = "http://127.0.0.1:3000/employees/";
+
+    $.ajax({
+        type: "POST",
+        url: url,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: JSON.stringify(
+            {
+                "full_name": full_name,
+                "position": position,
+                "legal_hours": legal_hours,
+                "total_hours": total_hours,
+                "allocated_hours": allocated_hours,
+                "outsourced": outsourced,
+                "local": local,
+                "isActive": isActive
+            }
+        )
+    });
+
+    
+}
+
+
 function delEmployee(id) {
     var url = `http://127.0.0.1:3000/employees/${id}`;
 
@@ -86,6 +121,68 @@ function delEmployee(id) {
     window.location.href = window.location.href;
 }
 
+
+function patchEmp(id) {
+    document.getElementById("id").placeholder = id;
+
+    var url = `http://127.0.0.1:3000/employees/${id}`;
+    var res;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url, false);
+    xhttp.send();//A execução do script pára aqui até a requisição retornar do servidor
+
+    res = JSON.parse(xhttp.responseText);
+
+    document.getElementById("id").value = id;
+    document.getElementById("full_name").value = res[0].full_name;
+    document.getElementById("position").value = res[0].position;
+    document.getElementById("legal_hours").value = res[0].legal_hours;
+    document.getElementById("total_hours").value = res[0].total_hours;
+    document.getElementById("allocated_hours").value = res[0].allocated_hours;
+    document.getElementById("outsourced").value = res[0].outsourced;
+    document.getElementById("local").value = res[0].local;
+
+    document.getElementById("addBtn-Add").style.display = "none";
+    document.getElementById("patchBtn-Patch").style.display = "block";
+
+    document.getElementById("addBtn").setAttribute( "onClick", "javascript: patchEmpII();" );
+}
+
+function patchEmpII() {
+    var id = document.getElementById("id").value;
+    var full_name = document.getElementById("full_name").value;
+    var position = document.getElementById("position").value;
+    var legal_hours = document.getElementById("legal_hours").value;
+    var total_hours = document.getElementById("total_hours").value;
+    var allocated_hours = document.getElementById("allocated_hours").value;
+    var outsourced = parseInt(document.getElementById("outsourced").value);
+    var local = document.getElementById("local").value;
+    var isActive = 1;
+
+    var url = "http://127.0.0.1:3000/employees/";
+
+    $.ajax({
+        type: "POST",
+        url: url,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: JSON.stringify(
+            {
+                "full_name": full_name,
+                "position": position,
+                "legal_hours": legal_hours,
+                "total_hours": total_hours,
+                "allocated_hours": allocated_hours,
+                "outsourced": outsourced,
+                "local": local,
+                "isActive": isActive
+            }
+        )
+    });
+
+    delEmp(id);
+}
 
 function updateForm(id) {
     var url = `http://127.0.0.1:3000/employees/${id}`;
